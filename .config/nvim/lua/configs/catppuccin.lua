fc0539bf27f71a4edafd18616f392a0f91c49e9b
selectgender <sylviaviajung@gmail.com>
2025-01-07 09:14:50 -0800

    removed some configs i dont use

--- .config/nvim/lua/configs/catppuccin.lua
+++
@@ -1,9 +0,0 @@
-local catppuccin = {}
-
-catppuccin.opts = {
-	-- flavour = "macchiato", -- latte, frappe, macchiato, mocha
-	flavour = "mocha", -- latte, frappe, macchiato, mocha
-	transparent_background = true, -- disables setting the background color.
-}
-
-return catppuccin
